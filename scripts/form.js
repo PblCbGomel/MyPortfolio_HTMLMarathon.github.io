c0dc63ee5012392b714f0483921a309eadc36ca5
Evgeniy Shevcov <yevgeniy.shevtsov.pblcb@mail.ru>
2021-10-04 01:07:55 +0300

    Few changes

---
+++ scripts/form.js
@@ -0,0 +1,10 @@
+function writeToFile() { 
+    var name = document.getElementById("fio");
+    var email = document.getElementById("email");
+    var text = document.getElementById("text");
+
+    alert("ФИО: " + name.value + "\nПочта: " + email.value + "\nСообщение: " + text.value);
+
+    location.replace('#');
+    location.reload();
+}
